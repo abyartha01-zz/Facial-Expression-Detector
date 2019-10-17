6bbca5773fb9264c3784343a1c1ba71958735dc0
Abyartha Goswami <abyartha01@gmail.com>
2019-10-17 13:01:51 +0530

    Update Facial_expression_detector_InceptionNet_training.py

--- Facial_expression_detector_InceptionNet_training.py
+++ Facial_expression_detector_InceptionNet_training.py
@@ -21,10 +21,10 @@ from keras.callbacks import ModelCheckpoint                                     
 
 # %% Importing pre-processed data
 
-X_train = np.load("Pre-processed_data//X_train.npy")                                                #loaded X_train
-Y_train = np.load("Pre-processed_data//Y_train.npy")                                                #loaded Y_train 
-X_test = np.load("Pre-processed_data//X_test.npy")                                                  #loaded X_test
-Y_test = np.load("Pre-processed_data//Y_test.npy")                                                  #loaded Y_train            
+X_train = np.load("X_train.npy")                                                #loaded X_train
+Y_train = np.load("Y_train.npy")                                                #loaded Y_train 
+X_test = np.load("X_test.npy")                                                  #loaded X_test
+Y_test = np.load("Y_test.npy")                                                  #loaded Y_train            
                            
 # %% Defining a function to return a inception module
 #    with required number of filters
